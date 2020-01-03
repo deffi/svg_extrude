feb3c93e79a9da93698c16c041fe223d53d5f52a
Martin Herrmann <martin@famherrmann.de>
2020-01-03 23:08:11 +0100

    Fix typo

--- test/model/test_color.py
+++ test/model/test_color.py
@@ -3,7 +3,7 @@ import unittest
 from svg2stl.model import Color
 
 
-class TestColo(unittest.TestCase):
+class TestColor(unittest.TestCase):
     def test_hsv(self):
         self.assertEqual(Color.from_hsv(0/6, 1, 1).rgb(), (1.0, 0.0, 0.0))
         self.assertEqual(Color.from_hsv(1/6, 1, 1).rgb(), (1.0, 1.0, 0.0))
