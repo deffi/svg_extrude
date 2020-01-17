fdc4c5a3e956e95ab860de907bb84babe7bcad3b
Martin Herrmann <martin@famherrmann.de>
2020-01-17 22:42:40 +0100

    Add OutputFile

--- manual_tests/stl2svg_test.py
+++ manual_tests/stl2svg_test.py
@@ -3,8 +3,7 @@ from os import path
 import cjlano_svg as svg
 
 from svg2stl.model import Shape, Group, Color
-from svg2stl.scad import File as ScadFile
-from svg2stl.util import with_remaining, groupby
+from svg2stl import OutputFile
 
 px = 25.4 / 96
 
@@ -31,40 +30,5 @@ groups = Group.by_color(shapes)
 
 print(f"Writing to {scad_file_name}")
 with open(scad_file_name, "w") as file:
-    scad_file = ScadFile(file)
-
-    # Define the points and paths for all shapes
-    for shape in shapes:
-        scad_file.assignment(shape.points_name(), shape.polygon.points)
-        for index, path in enumerate(shape.polygon.paths):
-            scad_file.assignment(shape.path_name(index), path)
-
-    # Create modules for the individual shapes
-    scad_file.blank_link()
-    for index, shape in enumerate(shapes):
-        with scad_file.define_module(shape.name):
-            scad_file.polygon(shape.polygon, shape.points_name(), shape.path_names())
-
-    # Create modules for the individual clipped shapes
-    scad_file.blank_link()
-    for shape, remaining in with_remaining(shapes):
-        with scad_file.define_module(shape.module_only_name()):
-            with scad_file.difference():
-                scad_file.instance(shape.name)
-                scad_file.instances(s.name for s in remaining)
-
-    # Create modules for the individual color groups
-    scad_file.blank_link()
-    for group in groups:
-        with scad_file.define_module(group.name):
-            # Implicit union
-            for shape in group.shapes:
-                scad_file.instance(shape.module_only_name())
-
-    # Instantiate the groups with their respective color
-    scad_file.blank_link()
-    for index, group in enumerate(groups):
-        with scad_file.color(group.color):
-            with scad_file.translate([0, 0, 0*index]):
-                with scad_file.extrude(thickness):
-                    scad_file.instance(group.name)
+    output_file = OutputFile(file)
+    output_file.write(shapes, groups[0:1], thickness)
---
+++ svg2stl/__init__.py
@@ -0,0 +1,1 @@
+from .output_file import OutputFile
