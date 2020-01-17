8834162ac5c537fd90aefc7b836088b625a0eb2d
Martin Herrmann <martin@famherrmann.de>
2020-01-17 22:42:53 +0100

    Clean up

--- svg2stl/model/group.py
+++ svg2stl/model/group.py
@@ -7,9 +7,9 @@ class Group:
         self.shapes = shapes
 
     @classmethod
-    def by_color(cls, shapes, *, colormap = lambda c:c):
-        def create_group(color, shapes):
-            return Group(f"group_{color.to_html()}", color, shapes)
+    def by_color(cls, shapes, *, colormap=lambda c: c):
+        def create_group(color, group_shapes):
+            return Group(f"group_{color.to_html()}", color, group_shapes)
 
         grouped = groupby(shapes, lambda shape: colormap(shape.color))
         return [create_group(color, shapes) for color, shapes in grouped.items()]
