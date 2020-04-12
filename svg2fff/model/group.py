4025a06fc83ab06f369d1ca81dd11b1d091c542e
Martin Herrmann <martin@famherrmann.de>
2020-04-12 12:04:32 +0200

    Remove group name

--- svg2fff/model/group.py
+++ svg2fff/model/group.py
@@ -6,15 +6,13 @@ from svg2fff.util import groupby
 
 @dataclass()
 class Group:
-    name: str
     color: Color
     shapes: List[Shape]
 
     @classmethod
     def by_color(cls, shapes: List[Shape], *, color_mapping=lambda c: c) -> List["Group"]:
         def create_group(color: Color, group_shapes: List[Shape]):
-            # TODO the name calculation should be in OutputFile
-            return Group(f"group_{color.display_name()}", color, group_shapes)
+            return Group(color, group_shapes)
 
         grouped = groupby(shapes, lambda shape: color_mapping(shape.color))
         return [create_group(color, shapes) for color, shapes in grouped.items()]
