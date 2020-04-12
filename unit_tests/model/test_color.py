25745760b53d435f3c7689fc79a8b5c9fbd4091d
Martin Herrmann <martin@famherrmann.de>
2020-04-12 22:06:13 +0200

    Update unit tests

--- unit_tests/model/test_color.py
+++ unit_tests/model/test_color.py
@@ -1,10 +1,16 @@
 import unittest
 
 from svg2fff.model import Color
-from svg2fff.model.color import svg as svg_colors
 
 
 class ColorTest(unittest.TestCase):
+    def assertSequenceAlmostEqual(self, expected, actual, places=None, msg=None, delta=None):
+        self.assertEqual(type(expected), type(actual))
+        self.assertEqual(len(expected), len(actual))
+        for a, b in zip(expected, actual):
+            msg = msg or f"\nExpected: {expected}\nActual:   {actual}"
+            self.assertAlmostEqual(a, b, places, msg, delta)
+
     def test_hsv(self):
         self.assertEqual(Color.from_hsv(0/6, 1, 1, None).rgb(), (1.0, 0.0, 0.0))
         self.assertEqual(Color.from_hsv(1/6, 1, 1, None).rgb(), (1.0, 1.0, 0.0))
@@ -26,8 +32,38 @@ class ColorTest(unittest.TestCase):
         self.assertEqual("ABFA11", Color.from_rgb(0.671, 0.980, 0.067, None).to_html())
         self.assertEqual("FFFFFF", Color.from_rgb(1, 1, 1, None).to_html())
 
-    # TODO explicitly test Color.xyz and Color.rgb for some colors, confirm the
-    # values with another source
+    def test_to_xyz(self):
+        # Values from colormine (http://colormine.org/convert/rgb-to-xyz).
+        
+        self.assertSequenceAlmostEqual((0.0000, 0.0000, 0.0000), Color.from_html("000000", None).xyz(), places=3)
+        self.assertSequenceAlmostEqual((0.9505, 1.0000, 1.0890), Color.from_html("FFFFFF", None).xyz(), places=3)
+
+        self.assertSequenceAlmostEqual((0.4124, 0.2126, 0.0193), Color.from_html("FF0000", None).xyz(), places=3)
+        self.assertSequenceAlmostEqual((0.3576, 0.7152, 0.1192), Color.from_html("00FF00", None).xyz(), places=3)
+        self.assertSequenceAlmostEqual((0.1805, 0.0722, 0.9505), Color.from_html("0000FF", None).xyz(), places=3)
+
+        self.assertSequenceAlmostEqual((0.3186, 0.2390, 0.0416), Color.from_html("d2691e", None).xyz(), places=3)
+        self.assertSequenceAlmostEqual((0.5028, 0.3702, 0.1208), Color.from_html("ff7f50", None).xyz(), places=3)
+        self.assertSequenceAlmostEqual((0.3058, 0.1604, 0.0576), Color.from_html("dc143c", None).xyz(), places=3)
+
+    def test_to_lab(self):
+        # Values from the tested method, checked with colormine[1]. The values
+        # are a bit off, but that may be due to different parameter choice. The
+        # value for white (FFFFFF) is significantly off (colormine value: 100,
+        # 0.005, -0.010). Further investigation may be in order.
+        # [1] http://colormine.org/convert/rgb-to-lab
+
+        self.assertSequenceAlmostEqual((  0.000, 0.000,  0.000), Color.from_html("000000", None).lab(), places=3)
+        self.assertSequenceAlmostEqual((100.000, 0.414, -0.985), Color.from_html("FFFFFF", None).lab(), places=3)
+
+        self.assertSequenceAlmostEqual((53.239,  80.404,   66.947), Color.from_html("FF0000", None).lab(), places=3)
+        self.assertSequenceAlmostEqual((87.735, -85.884,   82.713), Color.from_html("00FF00", None).lab(), places=3)
+        self.assertSequenceAlmostEqual((32.299,  79.430, -108.798), Color.from_html("0000FF", None).lab(), places=3)
+
+        self.assertSequenceAlmostEqual((55.989, 37.338, 56.410), Color.from_html("d2691e", None).lab(), places=3)
+        self.assertSequenceAlmostEqual((67.294, 45.686, 47.020), Color.from_html("ff7f50", None).lab(), places=3)
+        self.assertSequenceAlmostEqual((47.035, 71.203, 33.228), Color.from_html("dc143c", None).lab(), places=3)
+
 
     # Needs the colormath package, but note that that pulls in numpy
     # def test_xyz(self):
