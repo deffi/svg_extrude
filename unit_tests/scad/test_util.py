9f67d85640625ca591034b33fe58c6ad38daf3dd
Martin Herrmann <martin@famherrmann.de>
2020-04-13 17:10:58 +0200

    Fix failing unit tests

--- unit_tests/scad/test_util.py
+++ unit_tests/scad/test_util.py
@@ -6,17 +6,18 @@ from svg2fff.scad.util import render
 
 class TestUtil(unittest.TestCase):
     def test_render(self):
-        self.assertEqual(render(42), "42")
-        self.assertEqual(render(42.0), "42.0")
+        self.assertEqual("42", render(42))
+        self.assertEqual("42.0", render(42.0))
 
-        self.assertEqual(render([1, 2, 33]), "[1, 2, 33]")
-        self.assertEqual(render((1, 2, 33)), "[1, 2, 33]")
+        self.assertEqual("[1, 2, 33]", render([1, 2, 33]))
+        self.assertEqual("[1, 2, 33]", render((1, 2, 33)))
 
-        p1 = Point(11, 12)
-        p2 = Point(21, 22)
-        self.assertEqual(render(p1), "[11, 12]")
-        self.assertEqual(render(p2), "[21, 22]")
-        self.assertEqual(render([p1, p2]), "[[11, 12], [21, 22]]")
+        mm = 1e-3
+        p1 = Point(11 * mm, 12 * mm)
+        p2 = Point(21 * mm, 22 * mm)
+        self.assertEqual("[11.0, 12.0]", render(p1))
+        self.assertEqual("[21.0, 22.0]", render(p2))
+        self.assertEqual("[[11.0, 12.0], [21.0, 22.0]]", render([p1, p2]))
 
         with self.assertRaises(ValueError):
             render("foo")
