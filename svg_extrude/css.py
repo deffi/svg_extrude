e63a3f2199e72fa067b41829ed88a7931e41c258
Martin Herrmann <martin@famherrmann.de>
2020-04-20 22:07:31 +0200

    Initialize css.default_colors and css.colors from a list to make the order deterministic

--- svg_extrude/css.py
+++ svg_extrude/css.py
@@ -21,7 +21,7 @@ def extract_value(style: Optional[str], name: str) -> Optional[str]:
 
 # CSS colors from https://www.w3.org/TR/css-color-3/
 # noinspection SpellCheckingInspection
-default_colors = ColorSet({
+default_colors = ColorSet([
     Color.from_html("000000", "black"),
     Color.from_html("C0C0C0", "silver"),
     Color.from_html("808080", "gray"),
@@ -38,10 +38,10 @@ default_colors = ColorSet({
     Color.from_html("0000FF", "blue"),
     Color.from_html("008080", "teal"),
     Color.from_html("00FFFF", "aqua"),
-})
+])
 
 # noinspection SpellCheckingInspection
-colors = ColorSet({
+colors = ColorSet([
     Color.from_html("f0f8ff", "aliceblue"),
     Color.from_html("faebd7", "antiquewhite"),
     Color.from_html("00ffff", "aqua"),
@@ -189,4 +189,4 @@ colors = ColorSet({
     Color.from_html("f5f5f5", "whitesmoke"),
     Color.from_html("ffff00", "yellow"),
     Color.from_html("9acd32", "yellowgreen"),
-})
+])
